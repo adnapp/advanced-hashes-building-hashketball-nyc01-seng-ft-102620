5f0c29800721120968a85416ab490cb62fb38255
Adam Napoletano <adam.napps@gmail.com>
2020-10-19 18:52:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,6 @@
 # Write your code here!
+load 'pry'
+
 def game_hash
 basketball_data = {}
 basketball_data = {
@@ -6,15 +8,16 @@ basketball_data = {
     team_name: "Brooklyn Nets",
     colors: ["Black","White"],
     players: [
-      {player_name: 1},
-      {number: },
-      {shoe: },
-      {points: },
-      {rebounds: },
-      {assists: },
-      {steals: },
-      {blocks: },
-      {slam_dunks: },
+      {player_name: "Alan Anderson", "Reggie Evans", "Brook Lopez", "Mason Plumlee", "Jason Terry"},
+      {number: 0, 30, 11, 1, 31},
+      {shoe: 16, 14, 17, 19, 15},
+      {points: 22, 12, 17, 26, 19},
+      {rebounds: 12, 12, 19, 11, 2},
+      {assists: 12, 12, 10, 6, 2},
+      {steals: 3, 12, 3, 3, 4},
+      {blocks: 1, 12, 1, 8, 11},
+      {slam_dunks: 1, 7, 15, 5, 1},
+      binding.pry
     ]
   } ,
   away:{
