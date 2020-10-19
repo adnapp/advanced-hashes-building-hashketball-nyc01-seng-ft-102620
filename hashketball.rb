94ecb8249945c8ccda0b97a85e1d552c4f134f3b
Adam Napoletano <adam.napps@gmail.com>
2020-10-19 19:02:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -10,9 +10,9 @@ basketball_data = {
     players: [
       {player_name: "Alan Anderson", number: 0, shoe: 16, points: 22, rebounds: 12, assists:12, steals:3, blocks:1, slam_dunks:1}
     ]
-    binding.pry
+  }}
+  binding.pry
 
-  }} 
 #   away:{
 #     team_name: "",
 #     colors: ["",""],
