b4e3ec158e986857e44fe6a5d4ea4e20aaf1d9f4
Adam Napoletano <adam.napps@gmail.com>
2020-10-19 18:41:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,37 @@
 # Write your code here!
+def game_hash
+basketball_data = {}
+basketball_data = {
+  home: {
+    team_name: "Brooklyn Nets",
+    colors: ["Black","White"],
+    players: [
+      {player_name: },
+      {number: },
+      {shoe: },
+      {points: },
+      {rebounds: },
+      {assists: },
+      {steals: },
+      {blocks: },
+      {slam_dunks: },
+    ]
+  } ,
+  away:{
+    team_name: "",
+    colors: ["",""],
+    players: [
+      {player_name: },
+      {number: },
+      {shoe: },
+      {points: },
+      {rebounds: },
+      {assists: },
+      {steals: },
+      {blocks: },
+      {slam_dunks: },
+    ]
+  }
+}
+
+end
